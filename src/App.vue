95965538941122fe33400431f0b7813f8fccddc5
Taciano da Hora <taciano.dev@outlook.com>
2024-03-08 21:20:24 -0300

    feat(ui): organize project structure within the body

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="background">
     <TheHeader />
-    <div class="content"></div>
+    <MainTitle />
+    <div class="container">
+      <!-- Conteúdo do container aqui -->
+      <p>Este é um container com um fundo diferente e uma sombra.</p>
+    </div>
   </div>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
+import MainTitle from "./components/MainTitle.vue";
 
 export default {
   name: "App",
   components: {
     TheHeader,
+    MainTitle,
   },
 };
 </script>
@@ -27,9 +33,18 @@ export default {
 }
 
 body {
+  font-family: Arial, sans-serif;
   margin: 0;
   padding: 0;
-  font-family: Arial, sans-serif;
   background-color: #e6e6e6; /* Cor de fundo do body */
+}
+
+.container {
+  background-color: white;
+  width: 80%;
+  margin: 50px auto; /* Espaçamento maior para o container */
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Sombra */
 }
 </style>
---
+++ src/components/MainTitle.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Título</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+h1 {
+  font-size: 48px;
+  color: #333;
+  text-align: center;
+  margin-top: 100px;
+  margin-bottom: 50px;
+}
+</style>
